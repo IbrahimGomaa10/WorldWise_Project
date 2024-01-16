ab1e3c3b25b1088f74b868ed9a2c17b620dd256d
Ibrahim Gomaa <44735388+IbrahimGomaa10@users.noreply.github.com>
2024-01-16 20:56:49 +0200

    Update CitiesContext.jsx

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -6,7 +6,7 @@ import {
   useCallback,
 } from "react";
 
-const BASE_URL = "http://localhost:9000";
+const BASE_URL = "https://world-wise-server.vercel.app";
 
 const CitiesContext = createContext();
 
